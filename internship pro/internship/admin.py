a82683d0c031671c3312c922954ec852b70c6c69
Murungi Angella <angellamurungi91@gmail.com>
2026-03-19 05:05:40 -0700

    Delete internship pro directory

--- internship pro/internship/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from .models import Student, Supervisor, Report, Feedback
-
-admin.site.register(Student)
-admin.site.register(Supervisor)
-admin.site.register(Report)
-admin.site.register(Feedback)
